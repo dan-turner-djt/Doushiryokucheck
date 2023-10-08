414036fef87aa996fb1e9f232f3bda77dab18f37
Dan Turner <dan.turner.djt@gmal.com>
2023-10-08 05:13:55 +0100

    use aws domain name

--- src/Components/TestForm/TestForm.tsx
+++ src/Components/TestForm/TestForm.tsx
@@ -156,7 +156,8 @@ const TestForm = (props: TestFormProps) => {
 		const fullVerbList: VerbInfo[] = props.fullVerbList;
 		const randomVerbInfo: VerbInfo =  fullVerbList[Math.floor(Math.random() * fullVerbList.length)];*/
 
-		const endpoint = "https://3.8.4.192:5000/question";
+		//djt-backend-serv.xyz
+		const endpoint = "https://ec2-13-42-230-216.eu-west-2.compute.amazonaws.com:5000/question";
 		fetch(endpoint)
 			.then((response: Response) => {
 				if (response.status === 200) {
