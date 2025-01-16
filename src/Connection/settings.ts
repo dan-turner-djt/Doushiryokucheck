0c8c93a7b58daef6b1735537061567c582f7e3ee
Dan Turner <dan.turner.djt@gmail.com>
2025-01-16 01:56:49 +0000

    update endpoint

--- src/Connection/settings.ts
+++ src/Connection/settings.ts
@@ -1,2 +1,2 @@
-export const ROOT_ENDPOINT = "https://djt-backend-serv.xyz:5000";
+export const ROOT_ENDPOINT = "https://drc-be.djt-backend-serv.xyz:5000";
 //export const ROOT_ENDPOINT = "http://localhost:5000";
